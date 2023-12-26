bd3d1bd123698e5cd66e000ab01f95d8a11a9aa7
Vishv Patel <139690991+ervip@users.noreply.github.com>
2023-12-26 19:24:45 +0530

    StartScreen to QuizScreen button logic added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/screens/quiz_screen.dart';
+import 'package:quiz_app/screens/result_screen.dart';
 
 import 'package:quiz_app/screens/start_screen.dart';
 
@@ -9,8 +11,50 @@ void main() {
         decoration: const BoxDecoration(
           color: Color.fromARGB(255, 4, 7, 58),
         ),
-        child: const StartScreen(),
+        child: const Center(
+          child: Quiz(),
+        ),
       ),
     ),
   ));
+}
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  late Widget screen;
+  late Widget startScreen;
+  late Widget quizScreen;
+  late Widget resultScreen;
+
+  _QuizState() {
+    startScreen = StartScreen(startQuiz);
+    screen = startScreen;
+  }
+
+  void _setScreen(Widget screen) {
+    setState(() {
+      this.screen = screen;
+    });
+  }
+
+  void startQuiz() {
+    quizScreen = const QuizScreen();
+    _setScreen(quizScreen);
+  }
+
+  void showResult() {
+    resultScreen = const ResultScreen();
+    _setScreen(resultScreen);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return screen;
+  }
 }
--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,39 +1,38 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  final void Function() startQuizFunction;
+  const StartScreen(this.startQuizFunction, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Image.asset('assets/images/quiz-logo.png', width: 300, height: 300),
-          const SizedBox(height: 50),
-          const Text(
-            'Learn flutter the fun way !!',
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset('assets/images/quiz-logo.png', width: 300, height: 300),
+        const SizedBox(height: 50),
+        const Text(
+          'Learn flutter the fun way !!',
+          style: TextStyle(
+            color: Colors.white,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        const SizedBox(height: 50),
+        OutlinedButton.icon(
+          onPressed: startQuizFunction,
+          icon: const Icon(
+            Icons.arrow_right_alt,
+            color: Colors.white,
+          ),
+          label: const Text(
+            'Begin',
             style: TextStyle(
               color: Colors.white,
-              fontStyle: FontStyle.italic,
             ),
           ),
-          const SizedBox(height: 50),
-          OutlinedButton.icon(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.arrow_right_alt,
-              color: Colors.white,
-            ),
-            label: const Text(
-              'Begin',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-          )
-        ],
-      ),
+        )
+      ],
     );
   }
 }
