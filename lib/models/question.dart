c6cede612e1f788340b9c2f7a8135df055e78f55
Vishv Patel <139690991+ervip@users.noreply.github.com>
2023-12-26 19:37:45 +0530

    questions added as a data with model

---
+++ lib/models/question.dart
@@ -0,0 +1,11 @@
+class Question {
+  final String question;
+  final List<String> options;
+  final int answer;
+
+  const Question({
+    required this.question,
+    required this.options,
+    required this.answer,
+  });
+}
