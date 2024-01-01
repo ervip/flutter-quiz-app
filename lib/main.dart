d717ed11e40ab66b015ce45be85e32b84d3e5040
Vishv Patel <139690991+ervip@users.noreply.github.com>
2024-01-01 11:07:51 +0530

    Quiz screen with all the functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/screens/quiz_screen.dart';
 import 'package:quiz_app/screens/result_screen.dart';
 
@@ -29,8 +30,10 @@ class _QuizState extends State<Quiz> {
   late Widget startScreen;
   late Widget quizScreen;
   late Widget resultScreen;
+  late List<String> selectedAnswers;
 
   _QuizState() {
+    selectedAnswers = [];
     startScreen = StartScreen(startQuiz);
     screen = startScreen;
   }
@@ -42,8 +45,15 @@ class _QuizState extends State<Quiz> {
   }
 
   void startQuiz() {
-    quizScreen = const QuizScreen();
+    quizScreen = QuizScreen(onSelectAnswer: saveAnswer);
     _setScreen(quizScreen);
+  }
+
+  void saveAnswer(String answer) {
+    selectedAnswers.add(answer);
+    if (selectedAnswers.length == questions.length) {
+      showResult();
+    }
   }
 
   void showResult() {
--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -1,15 +1,83 @@
 import 'package:flutter/material.dart';
 
+import 'package:quiz_app/data/questions.dart';
+
 class QuizScreen extends StatefulWidget {
-  const QuizScreen({super.key});
+  final void Function(String answer) onSelectAnswer;
+  const QuizScreen({super.key, required this.onSelectAnswer});
 
   @override
   State<QuizScreen> createState() => _QuizScreenState();
 }
 
 class _QuizScreenState extends State<QuizScreen> {
+  int currentIndex = 0;
+
+  void answerQuestion(String answer) {
+    setState(() {
+      widget.onSelectAnswer(answer);
+      currentIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text('Quiz Screen');
+    return Center(
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Q. ${questions[currentIndex].question}',
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 20),
+            ...questions[currentIndex].getOptions().asMap().entries.map(
+              (option) {
+                return OptionButton(
+                  optionNumber: String.fromCharCode(65 + option.key),
+                  optionText: option.value,
+                  onPressed: () => answerQuestion(option.value),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class OptionButton extends StatelessWidget {
+  final String optionText;
+  final String optionNumber;
+  final void Function() onPressed;
+
+  const OptionButton({
+    super.key,
+    required this.optionNumber,
+    required this.optionText,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: onPressed,
+      style: const ButtonStyle(
+        alignment: Alignment.centerLeft,
+      ),
+      child: Text(
+        '$optionNumber. $optionText',
+        style: const TextStyle(color: Colors.white),
+      ),
+    );
   }
 }
