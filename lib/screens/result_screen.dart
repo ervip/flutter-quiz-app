6db805e3582c29f8c09cf3d49fdfce0cf69388ab
Vishv Patel <139690991+ervip@users.noreply.github.com>
2024-01-03 19:22:55 +0530

    summary logic shifted to screen class

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,48 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
-import 'package:quiz_app/models/question.dart';
 
 class _Summary extends StatelessWidget {
-  final List<Question> questions;
-  final List<String> userAnswers;
-
-  const _Summary({required this.questions, required this.userAnswers});
-
-  Map<String, Object> processResult() {
-    Map<String, Object> summary = {};
-    summary['totalQuestions'] = questions.length;
-    summary['totalAnswered'] = userAnswers.length;
-    summary['totalCorrect'] = 0;
-    summary['totalWrong'] = 0;
-    summary['explained'] = [];
-
-    for (int i = 0; i < questions.length; i++) {
-      bool isCorrect = questions[i].checkOption(userAnswers[i]);
-      if (isCorrect) {
-        summary['totalCorrect'] = (summary['totalCorrect'] as int) + 1;
-      } else {
-        summary['totalWrong'] = (summary['totalWrong'] as int) + 1;
-      }
-      (summary['explained'] as List<dynamic>).add({
-        'index': i,
-        'question': questions[i].question,
-        'correct': questions[i].options[0],
-        'selected': userAnswers[i],
-        'true_or_false': isCorrect
-      });
-    }
-
-    return summary;
-  }
+  final Map<String, Object> summary;
+  const _Summary({required this.summary});
 
   @override
   Widget build(BuildContext context) {
-    Map<String, Object> summary = processResult();
-    return SizedBox(
-      height: 400,
-      child: ListView(
-        children: [
-          ...(summary['explained'] as List<dynamic>).map(
+    return ListView(
+      children: (summary['explained'] as List<dynamic>)
+          .map(
             (e) => ListTile(
               leading: Icon(
                 e['true_or_false'] as bool ? Icons.check : Icons.close,
@@ -58,16 +25,15 @@ class _Summary extends StatelessWidget {
                 ),
               ),
               subtitle: Text(
-                "*${e['correct']}* is the correct answer.",
+                ('You\'ve selected: ${e["selected"]}\n'
+                    '${e["correct"]} is the correct answer.'),
                 style: const TextStyle(
                   color: Color.fromARGB(255, 255, 255, 255),
-                  fontStyle: FontStyle.italic,
                 ),
               ),
             ),
           )
-        ],
-      ),
+          .toList(),
     );
   }
 }
@@ -79,25 +45,55 @@ class ResultScreen extends StatelessWidget {
   const ResultScreen(
       {super.key, required this.chosenAnswers, required this.restartHandler});
 
+  Map<String, Object> prepareSummary() {
+    Map<String, Object> summary = {};
+    summary['totalQuestions'] = questions.length;
+    summary['totalAnswered'] = chosenAnswers.length;
+    summary['totalCorrect'] = 0;
+    summary['totalWrong'] = 0;
+    summary['explained'] = [];
+
+    for (int i = 0; i < questions.length; i++) {
+      bool isCorrect = questions[i].checkOption(chosenAnswers[i]);
+      if (isCorrect) {
+        summary['totalCorrect'] = (summary['totalCorrect'] as int) + 1;
+      } else {
+        summary['totalWrong'] = (summary['totalWrong'] as int) + 1;
+      }
+      (summary['explained'] as List<dynamic>).add({
+        'index': i,
+        'question': questions[i].question,
+        'correct': questions[i].options[0],
+        'selected': chosenAnswers[i],
+        'true_or_false': isCorrect
+      });
+    }
+
+    return summary;
+  }
+
   @override
   Widget build(BuildContext context) {
+    Map<String, Object> summary = prepareSummary();
+
     return Center(
       child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 25),
+        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 100),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'RESULT',
-              style: TextStyle(
+            Text(
+              ('You have answered ${summary["totalCorrect"]} out of '
+                  '${summary["totalAnswered"]} questions correctly.'),
+              style: const TextStyle(
                 color: Colors.white,
                 fontSize: 16,
                 fontWeight: FontWeight.bold,
               ),
             ),
             const SizedBox(height: 20),
-            _Summary(questions: questions, userAnswers: chosenAnswers),
+            Expanded(child: _Summary(summary: summary)),
             const SizedBox(height: 20),
             OutlinedButton.icon(
               onPressed: restartHandler,
